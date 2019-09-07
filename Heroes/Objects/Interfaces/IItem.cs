3484c1ac2a0ec7a307e8fa558354d6f1eecec242
Ostap Manchur <lololka23@gmail.com>
2019-09-07 15:45:18 +0300

    Added folders Objects, Helpers Added Items

---
+++ Heroes/Objects/Interfaces/IItem.cs
@@ -0,0 +1,20 @@
+﻿using Heroes.Helpers.Interfaces;
+using Heroes.Objects.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heroes.Objects.Interfaces
+{
+    interface IItem
+    {
+        IDescription Description { get; set; }
+        List<IEffect> Effects { get; set; }
+        Types ItemType { get; set; }
+        bool IsClickable { get; set; }
+        
+        
+    }
+}
